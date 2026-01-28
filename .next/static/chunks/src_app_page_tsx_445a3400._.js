abe2f39ae7918f19db88347e0c75206e31296f60
gazie <gaziev.80is@gmail.com>
2026-01-29 00:02:19 +0300

    fix: update next.js to patched version and remove .next folder

--- .next/static/chunks/src_app_page_tsx_445a3400._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_fadf3a05._.js",
-  "static/chunks/node_modules_5ab3214f._.js"
-],
-    source: "dynamic"
-});
